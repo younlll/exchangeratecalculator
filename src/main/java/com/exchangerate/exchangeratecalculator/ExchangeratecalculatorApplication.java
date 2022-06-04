5d714c079ea0807a684f224654830da5c3eb3712
younlll <youn.lee96@gmail.com>
2022-06-04 14:36:38 +0900

    init: 최초 등록

---
+++ src/main/java/com/exchangerate/exchangeratecalculator/ExchangeratecalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.exchangerate.exchangeratecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeratecalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeratecalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exchangerate/exchangeratecalculator/ExchangeratecalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchangerate.exchangeratecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeratecalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
