0fb2f27e5ff9f4ee2651be60c76fcbaa5d87fd82
younlll <youn.lee96@gmail.com>
2022-06-12 17:17:27 +0900

    chore: 버그 픽스 ExchangeRateRequest

--- src/main/java/com/exchangerate/exchangeratecalculator/dto/ExchangeRateRequest.java
+++ src/main/java/com/exchangerate/exchangeratecalculator/dto/ExchangeRateRequest.java
@@ -3,17 +3,17 @@ package com.exchangerate.exchangeratecalculator.dto;
 import com.exchangerate.exchangeratecalculator.domain.Country;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class ExchangeRateRequest {
-    @NotBlank
+    @NotNull
     private Country remittanceCountry;
 
-    @NotBlank
+    @NotNull
     private Country recipientCountry;
 
     @Min(value = 0, message = "송금액은 0 이상이어야 합니다.")
